5858bd5fca41373adf71e9dfcbb76b0bc2b1e290
Adrian Helle <adriah@ifi.uio.no>
2017-02-11 17:22:20 +0100

    Moved strings for menu to localization.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 import sys
 import configparser
 import RPi.GPIO as GPIO
+import localization
 
 import main
 from api import CybApi
@@ -27,7 +28,7 @@ def setup():
     for lcd in bar_lcd, customer_lcd:
         lcd.tick_off()
         lcd.clean()
-        lcd.write("Loading system!")
+        lcd.write(startmessage)
 
     # Get the config
     config = configparser.ConfigParser()
@@ -51,7 +52,7 @@ def setup():
 
 
 def get_card_id():
-    write((bar_lcd, customer_lcd), "Waiting for card")
+    write((bar_lcd, customer_lcd), waiting_for_card)
 
     return main.getid()
 
@@ -61,26 +62,26 @@ def register_customer(card_uid):
     user_id = ""
     is_intern = False
 
-    choice = ChoiceMenu((bar_lcd,), "Is the person an intern?", ("Yes", "No")).menu()
-    if choice is "Yes":
+    choice = ChoiceMenu((bar_lcd,), is_intern, (ans_yes, ans_no)).menu()
+    if choice is ans_yes:
         is_intern = True
         while not user_id:
-            username = KeyboardMenu((bar_lcd, customer_lcd), "Username").menu()
+            username = KeyboardMenu((bar_lcd, customer_lcd), uname).menu()
             if not username:
                 return (None, None)  # Empty username means cancel
 
             user = api.get_user(username)
             if "detail" in user:  # If there is a detail, it means that we didn't get a match.
-                write((bar_lcd, customer_lcd), "User not found")
+                write((bar_lcd, customer_lcd), usr_not_found)
                 time.sleep(2)  # Give user some time to read
             else:
                 user_id = user["id"]
     elif choice is None:
         return (None, None)
 
-    write((bar_lcd, customer_lcd), "Registrating card")
+    write((bar_lcd, customer_lcd), reg_card)
     if api.register_card(card_uid, user_id, is_intern):
-        write((bar_lcd, customer_lcd), "Card registerd!")
+        write((bar_lcd, customer_lcd), card_registered)
         time.sleep(2)  # Give the user some time to read
 
     return (username, is_intern)
@@ -91,9 +92,9 @@ def get_customer(card_uid):
 
     # The card is not in the database
     if username is None:
-        write((customer_lcd,), "Not accepted")
-        choice = ChoiceMenu((bar_lcd,), "Unknown card", ("Register", "Cancel")).menu()
-        if choice is "Cancel" or choice is None:
+        write((customer_lcd,), not_accepted)
+        choice = ChoiceMenu((bar_lcd,), unknown, (reg_cho, cancel_cho)).menu()
+        if choice is cancel_cho or choice is None:
             return None
 
         username, is_intern = register_customer(card_uid)
@@ -113,9 +114,9 @@ def display_info(customer):
     output = ""
 
     if customer.intern:
-        output += "Name: %s\n" % customer.username
-        output += "Vouchers: %2d\n" % customer.vouchers
-    output += "Coffee: %2d" % customer.coffee_vouchers
+        output += "%s %s\n" % (name_inf, customer.username)
+        output += "%s %2d\n" % (vouch_ing, customer.vouchers)
+    output += "%s %2d" % (coffee_inf, customer.coffee_vouchers)
 
     write((bar_lcd,), output)
     # We don't want to display the username on the customer screen
@@ -125,21 +126,21 @@ def display_info(customer):
 
 
 def register_use(identifier, amount, api_method):
-    write((bar_lcd, customer_lcd), "Withdrawing %d vouchers" % amount)
+    write((bar_lcd, customer_lcd), do_withdraw % amount)
 
     if api_method(identifier, amount):
-        write((bar_lcd, customer_lcd), "%d vouchers withdrawn" % amount)
+        write((bar_lcd, customer_lcd), fin_withdraw % amount)
     else:
-        write((bar_lcd, customer_lcd), "Not enough vouchers")
+        write((bar_lcd, customer_lcd), not_enough)
 
 
 def register_vouchers(card_uid, amount):
-    write((bar_lcd, customer_lcd), "Adding %d vouchers" % amount)
+    write((bar_lcd, customer_lcd), adding_vouchers % amount)
 
     if api.register_coffee_vouchers(card_uid, amount):
-        write((bar_lcd, customer_lcd), "%d vouchers added" % amount)
+        write((bar_lcd, customer_lcd), fin_adding % amount)
     else:
-        write((bar_lcd, customer_lcd), "Wrong D:")
+        write((bar_lcd, customer_lcd), wrong_msg)
 
 
 def buy_action():
@@ -153,15 +154,15 @@ def buy_action():
     display_info(customer)
 
     # Get amount of bongs to remove
-    amount = AmountMenu((bar_lcd,), "Amount to withdraw", clean=False, position=3).menu()
+    amount = AmountMenu((bar_lcd,), amo_withdraw, clean=False, position=3).menu()
     if not amount:
         return
 
     # Remove x amount of bongs from customer
     voucher_type = None
     if customer.intern:
-        voucher_type = ChoiceMenu((bar_lcd,), "What type?", ("Internvoucher", "Coffeevoucher")).menu()
-    if voucher_type is "Internvoucher":
+        voucher_type = ChoiceMenu((bar_lcd,), what_type, (in_vouch, cof_vouch)).menu()
+    if voucher_type is in_vouch:
         register_use(customer.username, amount, api.use_vouchers)
     else:
         register_use(card_uid, amount, api.use_coffee_vouchers)
@@ -188,11 +189,11 @@ if __name__ == "__main__":
     while True:
         choice = ChoiceMenu(
             (bar_lcd,),
-            "What do you want to do?",
-            ("Withdraw", "Buy coffee vouchers")
+            what_do,
+            (withdraw, buy_coffee)
         ).menu()
 
-        if choice is "Withdraw":
+        if choice is withdraw:
             buy_action()
-        elif choice is "Buy coffee vouchers":
+        elif choice is buy_coffee:
             register_action()
